0860c3643d42fed7d5e82572b96e7047cdbebd87
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-10-30 20:01:40 -0300

    rotas de alterar todos implementadas

--- src/index.js
+++ src/index.js
@@ -53,14 +53,18 @@ app.post('/users', (request, response) => {
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { username } = request.headers;
 
-  return response.json(users.todos);
+  const user = users.find(user => user.username === username);
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
  const { title, deadline } = request.body;
  const { username } = request.headers;
 
- users.username.todos.push( {
+ const user = users.find(user => user.username === username);
+
+ user.todos.push( {
   id: uuidv4(),
 	title,
 	done: false, 
@@ -69,14 +73,39 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   }
 );
 
+
+
+return response.json(user.todos.slice(-1));
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  const { title, deadline } = request.body;
+  const { id } = request.query;
+
+  const user = users.find(user => user.username === username);
+
+  const todoId = user.todos.find(todoId => user.todos.id === id);
+
+  todoId.title = title;
+  todoId.deadline = new Date(deadline);
+
+  return response.json(todoId)
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.query;
+
+  const user = users.find(user => user.username === username);
+  const todoId = user.todos.find(todoId => user.todos.id === id);
+
+  todoId.done = true;
+  
+  return response.json(todoId)
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
